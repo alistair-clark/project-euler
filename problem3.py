fc30c5f258be21190e779c26152cee7da90c53b9
alistair-clark <alistair.jl.clark@gmail.com>
2019-05-01 07:51:19 -0700

    Update after save

--- problem3.py
+++ problem3.py
@@ -1,6 +1,24 @@
 '''
 Project Euler
-Problem #1: Multiples of 3 and 5
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-Find the sum of all the multiples of 3 or 5 below 1000.
+Problem #3: Largest prime factor
+The prime factors of 13195 are 5, 7, 13 and 29.
+What is the largest prime factor of the number 600851475143 ?
 '''
+
+def LargestPrimeFactor(num):
+    """
+    Input: num, a positive int
+    Returns the largest prime factor of num
+    """
+    factors = []
+    d = 2 # Setting at 2 since this is the first non-trivial factor
+    while num > 1:
+        while num % d == 0:
+            factors.append(d)
+            num /= d
+        d += 1
+
+    return max(factors)
+
+
+LargestPrimeFactor(600851475143)
